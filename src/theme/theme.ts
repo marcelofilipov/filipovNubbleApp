608b3dcef6756c76c92060d2bad1b107dd533233
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-11-22 20:35:45 -0300

    Box Component

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -37,7 +37,19 @@ export const theme = createTheme({
     success: palette.greenSuccess,
     successLight: palette.greenSuccessLight,
   },
-  spacing: {},
+  spacing: {
+    s4: 4,
+    s8: 8,
+    s12: 12,
+    s14: 14,
+    s16: 16,
+    s20: 20,
+    s24: 24,
+    s32: 32,
+    s40: 40,
+    s48: 48,
+    s56: 56,
+  },
   borderRadii: {},
 });
 
