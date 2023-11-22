c40670dffbedefc3c88c2fd83fff209c2b64b7b3
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-11-22 20:53:48 -0300

    Button - borderRadius property

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,14 +16,7 @@ export function Button({title, loading}: ButtonProps) {
       height={50}
       alignItems="center"
       justifyContent="center"
-      // style={{
-      //   paddingHorizontal: 20,
-      //   paddingVertical: 14,
-      //   backgroundColor: colors.carrotSecondary,
-      //   alignItems: 'center',
-      //   borderRadius: 16,
-      // }}
-    >
+      borderRadius="s16">
       {loading ? (
         <ActivityIndicator />
       ) : (
