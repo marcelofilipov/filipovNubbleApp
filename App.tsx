c65a841693574f6813c0acfbefcb40f0b4a3b941
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-11-25 18:13:57 -0300

    Introduction - TextInput Component

--- App.tsx
+++ App.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
 import {ThemeProvider} from '@shopify/restyle';
 import {Text} from './src/components/Text/Text';
-import {Button} from './src/components/Button/Button';
 import {theme} from './src/theme/theme';
-import {Icon} from './src/components/Icon/Icon';
+import {Box} from './src/components/Box/Box';
+import {Button} from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text italic preset="headingMedium">
-            NubbleApp
+          <Text mb="s8" preset="headingLarge">
+            Olá
+          </Text>
+          <Text mb="s40" preset="paragraphLarge">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <Box mb="s20">
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+          <Box>
+            <TextInput
+              placeholder="Digite sua senha"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+
+          <Text color="primary" preset="paragraphSmall" bold mt="s10">
+            Esqueci minha senha
           </Text>
 
-          <Button title="Primary" marginBottom="s12" />
-          <Button disabled title="Primary" marginBottom="s12" />
-          <Button preset="outline" title="Outline" marginBottom="s12" />
-          <Button
-            disabled
-            preset="outline"
-            title="Outline"
-            marginBottom="s12"
-          />
-
-          <Button loading title="Loading" />
-          <Icon name="eyeOn" color="error" size={40} />
-          <Icon name="eyeOff" color="carrotSecondary" size={40} />
-          <Icon name="arrowLeft" color="buttonPrimary" size={40} />
+          <Button mt="s48" title="Entrar" />
+          <Button preset="outline" mt="s12" title="Criar uma conta" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
