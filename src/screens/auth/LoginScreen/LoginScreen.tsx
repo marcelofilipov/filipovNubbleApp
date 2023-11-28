cedddc1073e38c022a53eb8867d50f0501b58fd7
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-11-27 22:46:23 -0300

    Correction - Removing <View />

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
 import {Button} from '../../../components/Button/Button';
 import {Text} from '../../../components/Text/Text';
 import {TextInput} from '../../../components/TextInput/TextInput';
@@ -9,35 +8,33 @@ import {Screen} from '../../../components/Screen/Screen';
 export function LoginScreen() {
   return (
     <Screen>
-      <View style={{paddingHorizontal: 24}}>
-        <Text mb="s8" preset="headingLarge">
-          Olá
-        </Text>
-        <Text mb="s40" preset="paragraphLarge">
-          Digite seu e-mail e senha para entrar
-        </Text>
+      <Text mb="s8" preset="headingLarge">
+        Olá
+      </Text>
+      <Text mb="s40" preset="paragraphLarge">
+        Digite seu e-mail e senha para entrar
+      </Text>
 
-        <TextInput
-          errorMessage="Mensagem de erro"
-          label="E-mail"
-          placeholder="Digite seu e-mail"
-          boxProps={{mb: 's20'}}
-        />
+      <TextInput
+        errorMessage="Mensagem de erro"
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's20'}}
+      />
 
-        <TextInput
-          label="Senha"
-          placeholder="Digite sua senha"
-          RightComponent={<Icon color="gray2" name="eyeOn" />}
-          boxProps={{mb: 's10'}}
-        />
+      <TextInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        RightComponent={<Icon color="gray2" name="eyeOn" />}
+        boxProps={{mb: 's10'}}
+      />
 
-        <Text color="primary" preset="paragraphSmall" bold>
-          Esqueci minha senha
-        </Text>
+      <Text color="primary" preset="paragraphSmall" bold>
+        Esqueci minha senha
+      </Text>
 
-        <Button mt="s48" title="Entrar" />
-        <Button preset="outline" mt="s12" title="Criar uma conta" />
-      </View>
+      <Button mt="s48" title="Entrar" />
+      <Button preset="outline" mt="s12" title="Criar uma conta" />
     </Screen>
   );
 }
