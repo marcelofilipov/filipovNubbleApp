a161ee3992c099b89715b07eeddd96f81d64a84e
Marcelo Filipov <marcelo.filipov@gmail.com>
2023-11-28 22:27:21 -0300

    Adding ScrollView

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -3,25 +3,43 @@ import {Box} from '../Box/Box';
 import {useAppSafeArea} from '../../hooks/useAppSafeArea';
 import {Icon} from '../Icon/Icon';
 import {Text} from '../Text/Text';
+import {KeyboardAvoidingView, Platform} from 'react-native';
+import {ScrolViewContainer, ViewContainer} from './components/ScreenContainer';
+import {useAppTheme} from '../../hooks/useAppThemes';
 
 interface ScreenProps {
   children: React.ReactNode;
   canGoBack?: boolean;
+  scrollable?: boolean;
 }
 
-export function Screen({children, canGoBack = false}: ScreenProps) {
-  const {top} = useAppSafeArea();
+export function Screen({
+  children,
+  canGoBack = false,
+  scrollable = false,
+}: ScreenProps) {
+  const {top, bottom} = useAppSafeArea();
+  const {colors} = useAppTheme();
+  const Container = scrollable ? ScrolViewContainer : ViewContainer;
   return (
-    <Box paddingHorizontal="s24" style={{paddingTop: top}}>
-      {canGoBack && (
-        <Box mb="s24" flexDirection="row">
-          <Icon name="arrowLeft" color="primary" />
-          <Text preset="paragraphMedium" semiBold ml="s8">
-            Voltar
-          </Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Container backgroundColor={colors.background}>
+        <Box
+          paddingBottom="s24"
+          paddingHorizontal="s24"
+          style={{paddingTop: top, paddingBottom: bottom}}>
+          {canGoBack && (
+            <Box mb="s24" flexDirection="row">
+              <Icon name="arrowLeft" color="primary" />
+              <Text preset="paragraphMedium" semiBold ml="s8">
+                Voltar
+              </Text>
+            </Box>
+          )}
+          {children}
         </Box>
-      )}
-      {children}
-    </Box>
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
