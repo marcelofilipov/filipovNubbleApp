7626889ddb03410ac7c740dd08d5f51d37a5a4fa
Marcelo Filipov <marcelo.filipov@gmail.com>
2024-07-15 18:19:32 -0300

    Fixing the Require Cycles warning

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -5,7 +5,8 @@ import {
   TextInputProps as RNTextInputProps,
   TextStyle,
 } from 'react-native';
-import {$fontFamily, $fontSizes, Box, BoxProps, Text} from '@components';
+import {$fontFamily, $fontSizes, Text} from '../Text/Text';
+import {Box, BoxProps} from '../Box/Box';
 import {useAppTheme} from '@hooks';
 
 export interface TextInputProps extends RNTextInputProps {
