42017daf1d58cf3f683fef655913cbe78bc43f6f
Marcelo Filipov <marcelo.filipov@gmail.com>
2024-08-21 22:38:51 -0300

    Integrating Service into the Home Screen

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,7 +2,7 @@ import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
-  //TODO: simular um delay na API
+  await new Promise(resolve => setTimeout(() => resolve(''), 2000));
   return postListMock;
 }
 
